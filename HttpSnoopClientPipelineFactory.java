6f581390a105f0551e18b2acbff5ec190f3713b7
BOBTommy <wnsgh611@gmail.com>
2012-12-25 04:42:09 +0900

    2

---
+++ HttpSnoopClientPipelineFactory.java
@@ -0,0 +1,43 @@
+package Snoop;
+
+import static org.jboss.netty.channel.Channels.*;
+
+import javax.net.ssl.SSLEngine;
+
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.http.HttpClientCodec;
+import org.jboss.netty.handler.codec.http.HttpContentDecompressor;
+import org.jboss.netty.handler.ssl.SslHandler;
+
+
+public class HttpSnoopClientPipelineFactory implements ChannelPipelineFactory{
+
+	private final boolean ssl;
+	
+	public HttpSnoopClientPipelineFactory(boolean ssl){
+		this.ssl = ssl;
+	}
+	
+	@Override
+	public ChannelPipeline getPipeline() throws Exception {
+		ChannelPipeline pipeline = pipeline();
+		
+		if(ssl){
+			SSLEngine engine = SecureChatSslContextFactory.getClientContext().createSSLEngine();
+			engine.setUseClientMode(true);
+			
+			pipeline.addLast("ssl", new SslHandler(engine));
+		}
+		
+		pipeline.addLast("clodec", new HttpClientCodec());
+		pipeline.addLast("inflator", new HttpContentDecompressor());
+		
+		pipeline.addLast("handler", new HttpSnoopClientHandler());
+		
+		return pipeline;
+	}
+
+	
+
+}
